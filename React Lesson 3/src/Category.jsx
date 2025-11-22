550af92f8826b8617da81fe67d6e11291b21a2f1
AslanSoft <beggyplayy@gmail.com>
2025-11-22 22:56:56 +0500

    New porducst added

--- React Lesson 3/src/Category.jsx
+++ React Lesson 3/src/Category.jsx
@@ -4,6 +4,10 @@ import kurtka from "./assets/img/kurtka.jpg";
 import kurtka2 from "./assets/img/kurtka2.jpg";
 import kurtka3 from "./assets/img/kurtka3.jpg";
 import kurtka4 from "./assets/img/kurtka4.jpg";
+import kurtka5 from "./assets/img/kurtka5.jpg";
+import kurtka6 from "./assets/img/kurtka6.jpg";
+import kurtka7 from "./assets/img/kurtka7.jpg";
+import kurtka8 from "./assets/img/kurtka8.jpg";
 
 function Category() {
   return (
@@ -41,35 +45,35 @@ function Category() {
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
         </div>
         <div className="variant">
-            <img className='shop-img' src={kurtka} alt="photo" style={{ width: "200px", height: "240px"}}/>
-            <p className='shop-p-second'>Куртка теплая женская мужская unisex зимняя y2k корейский стиль иссиқ аёллар эркаклар</p>
-            <p className='shop-p'>287 000</p>
+            <img className='shop-img' src={kurtka5} alt="photo" style={{ width: "200px", height: "240px"}}/>
+            <p className='shop-p-second'>Миттенка, Перчатки для женщин и девочек, Перчатки без пальцев, Теплые, Подарок, Аксес</p>
+            <p className='shop-p'>69 000</p>
             <p>⭐⭐⭐⭐</p>
-            <mark shop-mark>22900 сумм/мес</mark>
+            <mark shop-mark>5200 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
         </div>
         <div className="variant">
-            <img className='shop-img' src={kurtka2} alt="photo" style={{ width: "200px", height: "240px"}}/>
-            <p className='shop-p-second'>Куртка, утеплённая водоотталкивающая, маломерит на 1-1,5 размера, изучите табл</p>
-            <p className='shop-p'>192 010</p>
+            <img className='shop-img' src={kurtka6} alt="photo" style={{ width: "200px", height: "240px"}}/>
+            <p className='shop-p-second'>Женская зимняя куртка с клетчатой подкладкой, утеплённая, свободного кроя  Подарок Теп</p>
+            <p className='shop-p'>269 900</p>
             <p>⭐⭐⭐⭐</p>
-            <mark shop-mark>14095 сумм/мес</mark>
-            <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
-        </div>
-        <div className="variant-second">
-            <img className='shop-img' src={kurtka3} alt="photo" style={{ width: "200px", height: "240px"}}/>
-            <p className='shop-p-second'>Тапочки домашние иссиқ аёллар закрытые маломерит на 1-1,5 размера, изучите табл</p>
-            <p className='shop-p'>80 700</p>
-            <p>⭐⭐⭐⭐</p>
-            <mark shop-mark>6020 сумм/мес</mark>
+            <mark shop-mark>21 179 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
         </div>
         <div className="variant">
-            <img className='shop-img' src={kurtka4} alt="photo" style={{ width: "200px", height: "240px"}}/>
-            <p className='shop-p-second'>Женская зимняя куртка с капюшоном и меховой отделкой, с поясом и накладными карманами</p>
-            <p className='shop-p'>296 000</p>
+            <img className='shop-img' src={kurtka7} alt="photo" style={{ width: "200px", height: "240px"}}/>
+            <p className='shop-p-second'>Шапка-бини унисекс, шапка, не закрывающая ушей, доггер, зимняя, бини шапка</p>
+            <p className='shop-p'>45 700</p>
             <p>⭐⭐⭐⭐</p>
-            <mark shop-mark>28900 сумм/мес</mark>
+            <mark shop-mark>3470 сумм/мес</mark>
+            <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
+        </div>
+        <div className="variant">
+            <img className='shop-img' src={kurtka8} alt="photo" style={{ width: "200px", height: "240px"}}/>
+            <p className='shop-p-second'>Детская куртка демисезонная для мальчиков и девочек, на зиму пуховик</p>
+            <p className='shop-p'>168 000</p>
+            <p>⭐⭐⭐⭐</p>
+            <mark shop-mark>12 709сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
         </div>
     </div>
