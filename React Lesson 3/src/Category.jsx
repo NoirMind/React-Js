e0df3253ca2058b3946d082cb62c1bb75c7ad323
AslanSoft <beggyplayy@gmail.com>
2025-11-22 22:41:30 +0500

    divs ended

--- React Lesson 3/src/Category.jsx
+++ React Lesson 3/src/Category.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import kurtka from "./assets/img/kurtka.jpg";
 import kurtka2 from "./assets/img/kurtka2.jpg";
+import kurtka3 from "./assets/img/kurtka3.jpg";
+import kurtka4 from "./assets/img/kurtka4.jpg";
 
 function Category() {
   return (
@@ -16,18 +18,26 @@ function Category() {
         </div>
         <div className="variant">
             <img className='shop-img' src={kurtka2} alt="photo" style={{ width: "200px", height: "240px"}}/>
-            <p className='shop-p-second'>Куртка, утеплённая водоотталкивающая, маломерит на 1-1,5 размера, изучите таблицу размера</p>
+            <p className='shop-p-second'>Куртка, утеплённая водоотталкивающая, маломерит на 1-1,5 размера, изучите табл</p>
             <p className='shop-p'>192 010</p>
             <p>⭐⭐⭐⭐</p>
             <mark shop-mark>14095 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
         </div>
         <div className="variant">
-            <img className='shop-img' src={kurtka} alt="photo" style={{ width: "200px", height: "240px"}}/>
-            <p className='shop-p-second'>Куртка теплая женская мужская unisex зимняя y2k корейский стиль иссиқ аёллар эркаклар</p>
-            <p className='shop-p'>287 000</p>
+            <img className='shop-img' src={kurtka3} alt="photo" style={{ width: "200px", height: "240px"}}/>
+            <p className='shop-p-second'>Тапочки домашние иссиқ аёллар закрытые маломерит на 1-1,5 размера, изучите табл</p>
+            <p className='shop-p'>80 700</p>
             <p>⭐⭐⭐⭐</p>
-            <mark shop-mark>22900 сумм/мес</mark>
+            <mark shop-mark>6020 сумм/мес</mark>
+            <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
+        </div>
+        <div className="variant">
+            <img className='shop-img' src={kurtka4} alt="photo" style={{ width: "200px", height: "240px"}}/>
+            <p className='shop-p-second'>Женская зимняя куртка с капюшоном и меховой отделкой, с поясом и накладными карманами</p>
+            <p className='shop-p'>296 000</p>
+            <p>⭐⭐⭐⭐</p>
+            <mark shop-mark>28900 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
         </div>
         <div className="variant">
@@ -36,6 +46,30 @@ function Category() {
             <p className='shop-p'>287 000</p>
             <p>⭐⭐⭐⭐</p>
             <mark shop-mark>22900 сумм/мес</mark>
+            <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
+        </div>
+        <div className="variant">
+            <img className='shop-img' src={kurtka2} alt="photo" style={{ width: "200px", height: "240px"}}/>
+            <p className='shop-p-second'>Куртка, утеплённая водоотталкивающая, маломерит на 1-1,5 размера, изучите табл</p>
+            <p className='shop-p'>192 010</p>
+            <p>⭐⭐⭐⭐</p>
+            <mark shop-mark>14095 сумм/мес</mark>
+            <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
+        </div>
+        <div className="variant-second">
+            <img className='shop-img' src={kurtka3} alt="photo" style={{ width: "200px", height: "240px"}}/>
+            <p className='shop-p-second'>Тапочки домашние иссиқ аёллар закрытые маломерит на 1-1,5 размера, изучите табл</p>
+            <p className='shop-p'>80 700</p>
+            <p>⭐⭐⭐⭐</p>
+            <mark shop-mark>6020 сумм/мес</mark>
+            <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
+        </div>
+        <div className="variant">
+            <img className='shop-img' src={kurtka4} alt="photo" style={{ width: "200px", height: "240px"}}/>
+            <p className='shop-p-second'>Женская зимняя куртка с капюшоном и меховой отделкой, с поясом и накладными карманами</p>
+            <p className='shop-p'>296 000</p>
+            <p>⭐⭐⭐⭐</p>
+            <mark shop-mark>28900 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
         </div>
     </div>
