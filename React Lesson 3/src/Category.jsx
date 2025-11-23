70195e7b0cba9e3323df81fd39322b51b26809bf
AslanSoft <beggyplayy@gmail.com>
2025-11-23 20:56:02 +0500

    products ended

--- React Lesson 3/src/Category.jsx
+++ React Lesson 3/src/Category.jsx
@@ -13,6 +13,8 @@ import kurtka10 from "./assets/img/kurtka10.jpg";
 import kurtka11 from "./assets/img/kurtka11.jpg";
 import kurtka12 from "./assets/img/kurtka12.jpg";
 import kurtka13 from "./assets/img/kurtka13.jpg";
+import kurtka14 from "./assets/img/kurtka14.jpg";
+import kurtka15 from "./assets/img/kurtka15.jpg";
 
 function Category() {
   return (
@@ -20,7 +22,7 @@ function Category() {
         <div className="variant">
             <img className='shop-img' src={kurtka} alt="photo" style={{ width: "200px", height: "240px"}}/>
             <p className='shop-p-second'>Куртка теплая женская мужская unisex зимняя y2k корейский стиль иссиқ аёллар эркаклар</p>
-            <p className='shop-p'>287 000</p>
+            <p className='shop-p'>287 000 сум</p>
             <p>⭐⭐⭐⭐</p>
             <mark shop-mark>22900 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
@@ -28,7 +30,7 @@ function Category() {
         <div className="variant">
             <img className='shop-img' src={kurtka2} alt="photo" style={{ width: "200px", height: "240px"}}/>
             <p className='shop-p-second'>Куртка, утеплённая водоотталкивающая, маломерит на 1-1,5 размера, изучите табл</p>
-            <p className='shop-p'>192 010</p>
+            <p className='shop-p'>192 010 сум</p>
             <p>⭐⭐⭐⭐</p>
             <mark shop-mark>14095 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
@@ -36,7 +38,7 @@ function Category() {
         <div className="variant">
             <img className='shop-img' src={kurtka3} alt="photo" style={{ width: "200px", height: "240px"}}/>
             <p className='shop-p-second'>Тапочки домашние иссиқ аёллар закрытые маломерит на 1-1,5 размера, изучите табл</p>
-            <p className='shop-p'>80 700</p>
+            <p className='shop-p'>80 700 сум</p>
             <p>⭐⭐⭐⭐</p>
             <mark shop-mark>6020 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
@@ -44,7 +46,7 @@ function Category() {
         <div className="variant">
             <img className='shop-img' src={kurtka4} alt="photo" style={{ width: "200px", height: "240px"}}/>
             <p className='shop-p-second'>Женская зимняя куртка с капюшоном и меховой отделкой, с поясом и накладными карманами</p>
-            <p className='shop-p'>296 000</p>
+            <p className='shop-p'>296 000 сум</p>
             <p>⭐⭐⭐⭐</p>
             <mark shop-mark>28900 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
@@ -52,7 +54,7 @@ function Category() {
         <div className="variant">
             <img className='shop-img' src={kurtka5} alt="photo" style={{ width: "200px", height: "240px"}}/>
             <p className='shop-p-second'>Миттенка, Перчатки для женщин и девочек, Перчатки без пальцев, Теплые, Подарок, Аксес</p>
-            <p className='shop-p'>69 000</p>
+            <p className='shop-p'>69 000 сум</p>
             <p>⭐⭐⭐⭐</p>
             <mark shop-mark>5200 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
@@ -60,7 +62,7 @@ function Category() {
         <div className="variant">
             <img className='shop-img' src={kurtka6} alt="photo" style={{ width: "200px", height: "240px"}}/>
             <p className='shop-p-second'>Женская зимняя куртка с клетчатой подкладкой, утеплённая, свободного кроя  Подарок Теп</p>
-            <p className='shop-p'>269 900</p>
+            <p className='shop-p'>269 900 сум</p>
             <p>⭐⭐⭐⭐</p>
             <mark shop-mark>21 179 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
@@ -68,7 +70,7 @@ function Category() {
         <div className="variant">
             <img className='shop-img' src={kurtka7} alt="photo" style={{ width: "200px", height: "240px"}}/>
             <p className='shop-p-second'>Шапка-бини унисекс, шапка, не закрывающая ушей, доггер, зимняя, бини шапка</p>
-            <p className='shop-p'>45 700</p>
+            <p className='shop-p'>45 700 сум</p>
             <p>⭐⭐⭐⭐</p>
             <mark shop-mark>3470 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
@@ -76,7 +78,7 @@ function Category() {
         <div className="variant">
             <img className='shop-img' src={kurtka8} alt="photo" style={{ width: "200px", height: "240px"}}/>
             <p className='shop-p-second'>Детская куртка демисезонная для мальчиков и девочек, на зиму пуховик</p>
-            <p className='shop-p'>168 000</p>
+            <p className='shop-p'>168 000 сум</p>
             <p>⭐⭐⭐⭐</p>
             <mark shop-mark>12 709сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
@@ -84,7 +86,7 @@ function Category() {
          <div className="variant">
             <img className='shop-img' src={kurtka9} alt="photo" style={{ width: "200px", height: "240px"}}/>
             <p className='shop-p-second'>Кроссовки мужские, весна-осень-зима, непромокаемые, размер 39-44, демисезонная</p>
-            <p className='shop-p'>158 000</p>
+            <p className='shop-p'>158 000 сум</p>
             <p>⭐⭐⭐⭐</p>
             <mark shop-mark>11970 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
@@ -92,7 +94,7 @@ function Category() {
          <div className="variant">
             <img className='shop-img' src={kurtka10} alt="photo" style={{ width: "200px", height: "240px"}}/>
             <p className='shop-p-second'>Перчатки детские для девочки и мальчика Лабубу, 3-6 лет демисезонная</p>
-            <p className='shop-p'>33 100</p>
+            <p className='shop-p'>33 100 сум</p>
             <p>⭐⭐⭐⭐</p>
             <mark shop-mark>2290 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
@@ -100,7 +102,7 @@ function Category() {
          <div className="variant">
             <img className='shop-img' src={kurtka11} alt="photo" style={{ width: "200px", height: "240px"}}/>
             <p className='shop-p-second'>Женские резиновые сапоги-челси. Стильные непромокаемые ботинки, полусапожки</p>
-            <p className='shop-p'>151 050</p>
+            <p className='shop-p'>151 050 сум</p>
             <p>⭐⭐⭐⭐</p>
             <mark shop-mark>11262 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
@@ -108,7 +110,7 @@ function Category() {
         <div className="variant">
             <img className='shop-img' src={kurtka12} alt="photo" style={{ width: "200px", height: "240px"}}/>
             <p className='shop-p-second'>Женский Пуховик с чехлом SADO  Стильные непромокаемые весна-осень-зима</p>
-            <p className='shop-p'>359 100</p>
+            <p className='shop-p'>359 100 сум</p>
             <p>⭐⭐⭐⭐</p>
             <mark shop-mark>28262 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
@@ -116,25 +118,25 @@ function Category() {
          <div className="variant">
             <img className='shop-img' src={kurtka13} alt="photo" style={{ width: "200px", height: "240px"}}/>
             <p className='shop-p-second'>Женский трико, оверсайз  Стильные непромокаемые непромокаемые зима</p>
-            <p className='shop-p'>359 100</p>
+            <p className='shop-p'>144 090 сум</p>
             <p>⭐⭐⭐⭐</p>
-            <mark shop-mark>28262 сумм/мес</mark>
+            <mark shop-mark>10625 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
         </div>
         <div className="variant">
-            <img className='shop-img' src={kurtka12} alt="photo" style={{ width: "200px", height: "240px"}}/>
-            <p className='shop-p-second'>Женский Пуховик с чехлом SADO  Стильные непромокаемые весна-осень-зима</p>
-            <p className='shop-p'>359 100</p>
+            <img className='shop-img' src={kurtka14} alt="photo" style={{ width: "200px", height: "240px"}}/>
+            <p className='shop-p-second'>Женский тёплый спортивный костюм с худи, двойка, осень, зима, весна</p>
+            <p className='shop-p'>287 990 сум</p>
             <p>⭐⭐⭐⭐</p>
-            <mark shop-mark>28262 сумм/мес</mark>
+            <mark shop-mark>21249 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
         </div>
         <div className="variant">
-            <img className='shop-img' src={kurtka12} alt="photo" style={{ width: "200px", height: "240px"}}/>
-            <p className='shop-p-second'>Женский Пуховик с чехлом SADO  Стильные непромокаемые весна-осень-зима</p>
-            <p className='shop-p'>359 100</p>
+            <img className='shop-img' src={kurtka15} alt="photo" style={{ width: "200px", height: "240px"}}/>
+            <p className='shop-p-second'>Свитшот женский SADO SADO  Стильные непромокаемые весна-осень-зима</p>
+            <p className='shop-p'>191 040 сум</p>
             <p>⭐⭐⭐⭐</p>
-            <mark shop-mark>28262 сумм/мес</mark>
+            <mark shop-mark>21179 сумм/мес</mark>
             <button class="shopBtn"><i class="fa-solid fa-bag-shopping"></i>Savat</button>
         </div>
     </div>
